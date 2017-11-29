916544d2c2a9764dc5aa5222528faf400c18321b
PakyZG <33397510+PakyZG@users.noreply.github.com>
2017-11-30 00:36:09 +0100

    30.11.17.

---
+++ DaddyBot/Modules/Audio/AudioModule.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Daddy.Modules.Audio
+{
+    public class AudioModule : ModuleBase
+    {
+        // Scroll down further for the AudioService.
+        // Like, way down
+        private readonly AudioService _service;
+
+        // Remember to add an instance of the AudioService
+        // to your IServiceCollection when you initialize your bot
+        public AudioModule(AudioService service)
+        {
+            _service = service;
+        }
+
+        // You *MUST* mark these commands with 'RunMode.Async'
+        // otherwise the bot will not respond until the Task times out.
+        [Command("join", RunMode = RunMode.Async)]
+        public async Task JoinCmd()
+        {
+            await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
+        }
+
+        // Remember to add preconditions to your commands,
+        // this is merely the minimal amount necessary.
+        // Adding more commands of your own is also encouraged.
+        [Command("leave", RunMode = RunMode.Async)]
+        public async Task LeaveCmd()
+        {
+            await _service.LeaveAudio(Context.Guild);
+        }
+
+        [Command("play", RunMode = RunMode.Async)]
+        public async Task PlayCmd(string song)
+        {
+            await _service.SendAudioAsync(Context.Guild, Context.Channel, song);
+        }
+
+        //[Command("play", RunMode = RunMode.Async)]
+        public async Task play(string url)
+        {
+            await _service.SendAudioAsync(Context.Guild, Context.Channel, url);
+        }
+
+        [Command("yt", RunMode = RunMode.Async)]
+        public async Task ytPlayCmd(string url)
+        {
+            await _service.ytSendAudioAsync(Context.Guild, Context.Channel, url);
+        }
+
+        [Command("stream", RunMode = RunMode.Async), RequireUserPermission(GuildPermission.ManageChannels)]
+        public async Task Stream(string url)
+        {
+            try
+            {
+                var voicechannel = (Context.Message.Author as IGuildUser)?.VoiceChannel;
+                await _service.JoinAudio(voicechannel.Guild, voicechannel);
+                await _service.SendStreamAsync(voicechannel.Guild, Context.Channel, _service.GetvidUrl(url));
+                await _service.LeaveAudio(voicechannel.Guild);
+            }
+            catch (Exception ex)
+            {
+                await Main.Daddy.log("Stream error", LogSeverity.Error, "AudioModule", ex);
+            }
+
+        }
+    }
+}
---
+++ DaddyBot/Modules/Audio/AudioService.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+
+namespace Daddy.Modules.Audio
+{
+    public class AudioService
+    {
+        private readonly ConcurrentDictionary<ulong, IAudioClient> ConnectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
+
+        public async Task JoinAudio(IGuild guild, IVoiceChannel target)
+        {
+            IAudioClient client;
+            if (ConnectedChannels.TryGetValue(guild.Id, out client))
+            {
+                return;
+            }
+            if (target.Guild.Id != guild.Id)
+            {
+                return;
+            }
+
+            var audioClient = await target.ConnectAsync();
+
+            if (ConnectedChannels.TryAdd(guild.Id, audioClient))
+            {
+                // If you add a method to log happenings from this service,
+                // you can uncomment these commented lines to make use of that.
+                //await Log(LogSeverity.Info, $"Connected to voice on {guild.Name}.");
+            }
+        }
+
+        public async Task LeaveAudio(IGuild guild)
+        {
+            IAudioClient client;
+            if (ConnectedChannels.TryRemove(guild.Id, out client))
+            {
+                await client.StopAsync();
+            }
+        }
+
+        #region File Player
+
+        public async Task SendAudioAsync(IGuild guild, IMessageChannel channel, string path)
+        {
+            // Your task: Get a full path to the file if the value of 'path' is only a filename.
+            if (!File.Exists(path))
+            {
+                await channel.SendMessageAsync("File does not exist.");
+                return;
+            }
+            IAudioClient client;
+            if (ConnectedChannels.TryGetValue(guild.Id, out client))
+            {
+                //await Log(LogSeverity.Debug, $"Starting playback of {path} in {guild.Name}");
+                using (var output = CreateStream(path).StandardOutput.BaseStream)
+                using (var stream = client.CreatePCMStream(AudioApplication.Music))
+                {
+                    try { await output.CopyToAsync(stream); }
+                    finally { await stream.FlushAsync(); }
+                }
+            }
+        }
+
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",//-i {path} -ac 2 -f s16le -ar 48000 pipe:1
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+        }
+
+#endregion
+
+        #region Piping
+
+        public async Task ytSendAudioAsync(IGuild guild, IMessageChannel channel, string url)
+        {
+            IAudioClient client;
+            if (ConnectedChannels.TryGetValue(guild.Id, out client))
+            {
+                //await Log(LogSeverity.Debug, $"Starting playback of {path} in {guild.Name}");
+                using (var output = CreateUrlStream(url).StandardOutput.BaseStream)
+                using (var stream = client.CreatePCMStream(AudioApplication.Music, 128 * 1024))
+                {
+                    try { await output.CopyToAsync(stream); }
+                    finally { await stream.FlushAsync().ConfigureAwait(false); }
+                }
+            }
+        }
+
+        private Process CreateUrlStream(string url)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "youtube-dl",
+                Arguments = $"-o - \"{url}\" | ffmpeg -hide_banner -i pipe:0 -ac 2 -f s16le -ar 48000 pipe:1",//-o - {url} | ffmpeg -i pipe:0 -ac 2 -f s16le -ar 48000 pipe:1
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            });
+        }
+
+        #endregion
+
+        #region Youtube-dl -> ffmpeg
+
+        public string GetvidUrl(string url)
+        {
+            return Process.Start(new ProcessStartInfo()
+            {
+                FileName = @"youtube-dl",
+                Arguments = $" -x -g \"{url}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            }).StandardOutput.ReadLine();
+        }
+
+        private Process CreateStreamM(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = @"ffmpeg",
+                Arguments = $" -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1 -aq 2",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+        }
+
+        public async Task SendStreamAsync(IGuild guild, IMessageChannel channel, string path)
+        {
+            IAudioClient client;
+            if (ConnectedChannels.TryGetValue(guild.Id, out client))
+            {
+                //await Log(LogSeverity.Debug, $"Starting playback of {path} in {guild.Name}");
+                using (var output = CreateStreamM(path).StandardOutput.BaseStream)
+                using (var stream = client.CreatePCMStream(AudioApplication.Music, 128 * 1024))
+                {
+                    try { await output.CopyToAsync(stream); }
+                    finally { await stream.FlushAsync(); }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
