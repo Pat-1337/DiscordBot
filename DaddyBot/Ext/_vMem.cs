909b0b7a68ff90cf3dd97a35450b9ce173f11f6f
PakyZG <33397510+PakyZG@users.noreply.github.com>
2018-03-18 14:48:41 +0100

    18.3.18.

--- DaddyBot/Ext/_vMem.cs
+++ DaddyBot/Ext/_vMem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Discord;
@@ -13,7 +13,13 @@ namespace Daddy.Ext
 {
     public class _vMem
     {
-        public static Dictionary<ulong, Dictionary<object, object>> _vMemory = new Dictionary<ulong, Dictionary<object, object>>();
+        public static Dictionary<ulong, Dictionary<Modules.BaseCommands.Settings, object>> _vMemory = new Dictionary<ulong, Dictionary<Modules.BaseCommands.Settings, object>>();
+        public static _vMem instance;
+
+        public void Start()
+        {
+            instance = this;
+        }
 
         public void _run()
         {
@@ -24,16 +30,16 @@ namespace Daddy.Ext
         {
             _vMemory.Clear();
             Main.Daddy._client.Guilds.ToList().ForEach(x => {
-                Dictionary<object, object> _cache = new Dictionary<object, object>();
-                Dictionary<string, Dictionary<Modules.BaseCommands.Settings, string>> _json = new Dictionary<string, Dictionary<Modules.BaseCommands.Settings, string>>();
+                Dictionary<Modules.BaseCommands.Settings, object> _cache = new Dictionary<Modules.BaseCommands.Settings, object>();
+                Dictionary<string, Dictionary<Modules.BaseCommands.Settings, object>> _json = new Dictionary<string, Dictionary<Modules.BaseCommands.Settings, object>>();
                 try {
-                    _json = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<Modules.BaseCommands.Settings, string>>>(File.ReadAllText($@"Settings/settings_{x.Id}.json"));
+                    _json = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<Modules.BaseCommands.Settings, object>>>(File.ReadAllText($@"Settings/settings_{x.Id}.json"));
                 }
                 catch {
-                    if (!Modules.BaseCommands.doesExistSettings(Main.Daddy._client.GetGuild(x.Id) as IGuild)) {
-                        Modules.BaseCommands.createSettings(Main.Daddy._client.GetGuild(x.Id) as IGuild);
+                    if (!Modules.BaseCommands.DoesExistSettings(Main.Daddy._client.GetGuild(x.Id) as IGuild)) {
+                        Modules.BaseCommands.CreateSettings(Main.Daddy._client.GetGuild(x.Id) as IGuild);
                     }
-                    _json = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<Modules.BaseCommands.Settings, string>>>(File.ReadAllText($@"Settings/settings_{x.Id}.json"));
+                    _json = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<Modules.BaseCommands.Settings, object>>>(File.ReadAllText($@"Settings/settings_{x.Id}.json"));
                 }
                 foreach (Modules.BaseCommands.Settings setting in Enum.GetValues(typeof(Modules.BaseCommands.Settings))) {
                     try {
